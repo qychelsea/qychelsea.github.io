509e3e2ae36fd00275e2fcd405b5a58bea638fe5
yqiu <qychelsea@hotmail.com>
2017-10-01 00:16:46 -0400

    process weather app - literal 2

--- 13-Weather-minimal/Weather-Minimal.js
+++ 13-Weather-minimal/Weather-Minimal.js
@@ -6,11 +6,17 @@
 
 var queryResult;
 var img = [];
+var textSizeLarge = 95;
+var textSizeSmall = 30;
+var textColour = 95, lineColour = 190, backgroundColour = 250;
+var gap = 20;
+var rectLength;
 
 function setup() {
     createCanvas(640,1136);
-    background(255);
+    background(backgroundColour);
     query();
+    rectLength = (width - 4 * gap) / 3;
 }
 
 function query() {
@@ -44,40 +50,64 @@ function gotData(data) {
 
     var currentWeather = queryResult.currently;
 
-    var xPos = 20;
-    var yPos = 670;
-    var yGap = 150;
-    var textSizeLarge = 95;
-    var textSizeSmall = 30;
-    var gap = 20;
-    var rectLength = (width - 4*gap) / 3;
-    var textColour = 95, lineColour = 190;
-
     imageMode(CENTER);
 
-    image(img[currentWeather.icon], width/2, 225, img.width/2.5, img.height/2.5);
+    image(img[currentWeather.icon], width / 2, 225, img.width, img.height);
     fill(textColour);
     textStyle(BOLD);
     textSize(textSizeSmall);
     textAlign(CENTER);
-    text("C A M B R I D G E",width/2 ,450);
+    text("C A M B R I D G E", width / 2, 450);
+
+    drawCurrently();
+}
+
+function mousePressed() {
+    drawHourly();
+}
+/*
+function mousePressed(){
+    drawDaily();
+}
+
+function mousePressed() {
+    drawCurrently();
+}*/
+
+function clearBottom() {
+    fill(backgroundColour);
+    noStroke();
+    rect(0, 470, width, height - 470);
 
     stroke(lineColour);
-    line(gap,475,width-gap,475);
-    line(gap,565,width-gap,565);
+    line(gap, 475, width - gap, 475);
+    line(gap, 565, width - gap, 565);
 
-    fill(230);
+    fill(245);
     noStroke();
     for (var i = 0; i<=2; i++){
         rect((i+1)*gap+i*rectLength,485,rectLength,70);
     }
 
-    fill(textColour);
+    fill(190);
     textSize(textSizeSmall);
+    textAlign(CENTER);
     textStyle(NORMAL);
     text("currently",gap+rectLength/2,525);
     text("hourly",gap*2+rectLength*1.5,525);
     text("daily",gap*3+rectLength*2.5,525);
+}
+function drawCurrently(){
+    clearBottom();
+    var currentWeather = queryResult.currently;
+    var xPos = 20;
+    var yPos = 670;
+    var yGap = 150;
+
+    fill(textColour);
+    textSize(textSizeSmall);
+    textStyle(NORMAL);
+    text("currently",gap+rectLength/2,525);
 
     textAlign(LEFT);
     textSize(textSizeLarge);
@@ -90,32 +120,85 @@ function gotData(data) {
 
     text(Math.round(currentWeather.windSpeed) + "mph", 30, yPos);
     yPos += yGap;
-/*
+}
 
-    text(Math.floor(currentWeather.) + "mph", 30, yPos);
-    yPos += yGap;
-*/
+function drawDaily() {
+    clearBottom();
+    var dailyWeather = queryResult.daily.data;
+    console.log(dailyWeather);
+    var yPos = 670;
+    var yGap = 70;
 
-    /*    textSize(textSizeSmall);
-        text("Weather", 20, yPos);
-        yPos += textSizeLarge;
-        textSize(textSizeLarge);
-        text(currentWeather.summary, 20, yPos);
-        yPos += yGap;
+    fill(textColour);
+    textSize(textSizeSmall);
+    textStyle(NORMAL);
 
+    text("daily",gap*3+rectLength*2.5,525);
 
-
+    for (var i = 1; i <= 7; i++) {
+        textAlign(LEFT);
         textSize(textSizeSmall);
-        text("Precipitation", 20, yPos);
-        yPos += textSizeLarge;
-        textSize(textSizeLarge);
-        text(currentWeather.precipIntensity + "%", 20, yPos);
-        yPos += yGap;
+        textStyle(BOLD);
+        fill(textColour);
 
+        text("day  " + i, 30, yPos);
+        image(img[dailyWeather[i].icon], width / 2, yPos - 10, 50, 37);
+
+        textAlign(RIGHT);
+        text(Math.round(dailyWeather[i].temperatureHigh), width - 100, yPos);
+
+        textStyle(NORMAL);
+        text(Math.round(dailyWeather[i].temperatureLow), width - 30, yPos);
+
+        yPos += yGap;
+    }
+}
+
+function drawHourly(){
+    clearBottom();
+    var hourlyWeather = queryResult.hourly.data;
+    console.log(hourlyWeather);
+    var yPos = 670;
+    var yGap = 70;
+    var h = hour();
+    var hTrack = hour();
+    var apm = "am";
+
+    fill(textColour);
+    textSize(textSizeSmall);
+    textAlign(CENTER);
+    textStyle(NORMAL);
+    text("hourly",gap*2+rectLength*1.5,525);
+
+
+    if (hour()>=12){
+        apm = "pm";
+        if (hour()>12) {
+            h = hour() - 12;
+        }
+    }
+
+    for (var i = 1;i<=7;i++){
+        textAlign(LEFT);
         textSize(textSizeSmall);
-        text("Humidity", 20, yPos);
-        yPos += textSizeLarge;
-        textSize(textSizeLarge);
-        text(currentWeather.humidity + "%", 20, yPos);
-        yPos += yGap;*/
+        textStyle(BOLD);
+        fill(textColour);
+        if(hTrack>23) {
+            if (hTrack===24){
+                h=12;
+            }else {
+                h = hTrack - 24;
+            }
+            apm = "am";
+        }
+        text(h + " " + apm, 30, yPos);
+        textAlign(RIGHT);
+        text(Math.round(hourlyWeather[hTrack].temperature), width-40, yPos);
+
+        image(img[hourlyWeather[hTrack].icon], width/2, yPos-10, 50,37);
+
+        yPos += yGap;
+        hTrack++;
+        h++;
+    }
 }
