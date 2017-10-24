1d8edc9f8a3cdfb33ab882714f19407fa3767023
qychelseaPC <31788061+qychelsea@users.noreply.github.com>
2017-10-24 00:37:00 -0400

    Working Map

--- 15-Census/Census-V1.js
+++ 15-Census/Census-V1.js
@@ -2,8 +2,18 @@ var table = [];
 var esTab = [],empSzes = [],zipCode = [];
 var colour212,colour220,colour230,colour241,colour242,colour251,colour252,colour254,colour260;
 
+var map = L.map('map').setView([36.188574, -86.775464],11);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/qychelsea/cj8uawbmy99052rmiuzyyjg7b/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicXljaGVsc2VhIiwiYSI6ImNpcHFhZ3d3ODAwNXlod25wbDA2eWZta3IifQ.hEGkyGTCvHlqBm14G4pzxA', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.moonlight'
+}).addTo(map);
+
 //MapBox Token
-//pk.eyJ1IjoicXljaGVsc2VhIiwiYSI6ImNpcHFhZ3d3ODAwNXlod25wbDA2eWZta3IifQ.hEGkyGTCvHlqBm14G4pzxA
+//pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw
 
 
 function preload(){
@@ -68,3 +78,115 @@ function drawData(){
     }
     yPos=longestStr+50;
 }
+
+//-------------------LEAFLET CONTROL-----------------------------------------
+// control that shows state info on hover
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Nashville by ZIP Code</h4>' +  (props ?
+        '<b>' + props.zip + '</b><br />'
+        : 'Hover over to view area');
+};
+
+info.addTo(map);
+
+
+// get color depending on population density value
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+        d > 500  ? '#BD0026' :
+            d > 200  ? '#E31A1C' :
+                d > 100  ? '#FC4E2A' :
+                    d > 50   ? '#FD8D3C' :
+                        d > 20   ? '#FEB24C' :
+                            d > 10   ? '#FED976' :
+                                '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        weight: 1.5,
+        opacity: 1,
+        color: '#666666',
+        dashArray: 2,
+        fillOpacity: 0.35,
+        fillColor: '#ff99cc'
+        //fillColor: getColor(feature.properties.density)
+        //ff99cc (pink),527a7a9(purple grey),52527a(green grey)
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 2,
+        color: '#555',
+        dashArray: '',
+        fillOpacity: 0.65
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+var geojson;
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(nashvilleZipCodes, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+map.attributionControl.addAttribution('ZIP Code data &copy; <a href="https://data.nashville.gov/General-Government/Zip-Codes-GIS-/u7r5-bpku">Data.Nashville.gov</a>');
+
+
+var legend = L.control({position: 'bottomright'});
+
+/*legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        labels = [],
+        from, to;
+
+    for (var i = 0; i < grades.length; i++) {
+        from = grades[i];
+        to = grades[i + 1];
+
+        labels.push(
+            '<i style="background:' + getColor(from + 1) + '"></i> ' +
+            from + (to ? '&ndash;' + to : '+'));
+    }
+
+    div.innerHTML = labels.join('<br>');
+    return div;
+};*/
+
+//legend.addTo(map);
