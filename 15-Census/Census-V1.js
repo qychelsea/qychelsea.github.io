9757a9cd6fd8944b3f3736d64ce8e3398a20d1df
qychelseaPC <31788061+qychelsea@users.noreply.github.com>
2017-10-26 19:43:06 -0400

    mouseX control done

--- 15-Census/Census-V1.js
+++ 15-Census/Census-V1.js
@@ -1,6 +1,5 @@
 var table = [];
 var esTab = [],empSzes = [],zipCode=[];
-var colour212,colour220,colour230,colour241,colour242,colour251,colour252,colour254,colour260;
 var canvasX=605,canvasY = 0,marginSlider=100;
 var zipCurrent;
 var mx;
@@ -16,7 +15,6 @@ function preload(){
 function setup() {
     var canvas = createCanvas(2*marginSlider+18*lineLength+17*xGap, 900);
     canvas.position(canvasX,canvasY);
-    console.log("width=",width);
     sortData();
 
 }
@@ -34,78 +32,16 @@ function sortData(){
 }
 
 function draw(){
-    var c=0;
-    var xPos=marginSlider,yPos=height -150;
-    var mouseYear;
-    var strokeWeightCurrent = 1;
-
-    if(mouseX>marginSlider&&mouseX<width-marginSlider){
-        mouseYear = Math.floor(map(mouseX,marginSlider, width-marginSlider-lineLength,1998,2015));
-        console.log("mouseYear=",mouseYear,"winMouseX=",mouseX);
-        for(var n = 0;n<=esTab[mouseYear].length;n++){
-            if (zipCode[mouseYear][n]===zipCurrent+']'){
-                for(var j = n+9;j>n;j--){//draw from the thickest; goes through 260-212
-                    strokeWeight(strokeWeightCurrent);
-                    stroke(color(c,90,130));
-                    for (var k=0;k<esTab[mouseYear][j];k++){
-                        xPos=marginSlider+(mouseYear-1998)*(lineLength+xGap);
-                        line(xPos,yPos,xPos+lineLength,yPos);
-                        yPos=yPos-strokeWeightCurrent-1;
-                    }
-                    c=c+30;
-                }
-                break;
-            }
-        }
-    }
-
-}
-
-
-function drawData(e){
-    //if (eventCount==0){drawSlider();eventCount=1;}
     clear();
     background('#e8e3e8');
+    var c=0,a=75; //c for colour, a for alpha
     var xPos=marginSlider,yPos=height -150;
+    var mouseYear;
+    var strokeWeightCurrent = 2;
 
-    var longestStr;
-    var c,a=75;//c for colour, a for alpha
-
-    strokeCap(SQUARE);
-/*  //from before; draw all; original set xPos=50,yPos=100,xGap=50,yGap=1;
-    for(var i = 1998;i<=2015;i++){
-        stroke(c);
-        longestStr=0;
-        for(var n = 0;n<=esTab[i].length;n++){
-            if (empSzes[i][n]==1) {
-                xPos = xPos + lineLength + 5;
-                yPos = 100;
-                /!*if (longestStr<esTab[i][n]) {
-                    console.log("i=",i,"n=",n,"esTab[i][n]=",esTab[i][n],"empSzes[i][n]=",empSzes[i][n],"old longest string=",longestStr);
-                    longestStr = esTab[i][n];
-                    console.log("new longest string=", longestStr);
-                }*!/  //longest string tracking not working
-            }
-            if (empSzes[i][n]!=1){
-                //console.log("i=",i,"n=",n,"esTab[i][n]=",esTab[i][n],"empSzes[i][n]=",empSzes[i][n],",yPos=",yPos,"xPos=",xPos,"colour=",c);
-                for (var j=0;j<esTab[i][n];j++){
-                    //stroke(colour(empSzes[i][n]));
-                    line(xPos,yPos,xPos+lineLength,yPos);
-                    yPos=yPos+2;
-                }
-            }
-
-        }
-        c=c-30;
-    }
-    yPos=longestStr+50;*/
-
-    zipCurrent=e.target.feature.properties.zip;
-    console.log("zipCurrent=",zipCurrent);
-
+    //draw alpha
     for (var i = 1998;i<=2015;i++){
         c=0;
-        var strokeWeightCurrent = 1;
         longestStr=0;
         for(var n = 0;n<=esTab[i].length;n++){
             if (zipCode[i][n]===zipCurrent+']'){
@@ -121,39 +57,53 @@ function drawData(e){
                     c=c+30;
                 }
                 console.log("---");
-            break;
+                break;
             }
         }
         xPos= xPos + lineLength + xGap;
     }
 
+    //draw solid
+    yPos=height -150;
+    c=0;
+    if(mouseX>marginSlider&&mouseX<width-marginSlider){
+        mouseYear = Math.floor(map(mouseX,marginSlider, width-marginSlider-lineLength,1998,2015));
+        console.log("mouseYear=",mouseYear,"winMouseX=",mouseX);
+        for(n = 0;n<=esTab[mouseYear].length;n++){
+            if (zipCode[mouseYear][n]===zipCurrent+']'){
+                for(j = n+9;j>n;j--){//draw from the thickest; goes through 260-212
+                    strokeWeight(strokeWeightCurrent);
+                    stroke(color(c,90,130));
+                    for (k=0;k<esTab[mouseYear][j];k++){
+                        xPos=marginSlider+(mouseYear-1998)*(lineLength+xGap);
+                        line(xPos,yPos,xPos+lineLength,yPos);
+                        yPos=yPos-strokeWeightCurrent-1;
 
+                        push();//display value
+                        
+                        pop();
 
-/*    // SLIDER LABELS
-    n=-6;
-    for(i=1998;i<=2015; i++){
-        fill(75);
-        strokeWeight(1);
-        push();
-        translate(marginSlider+n,height-55);
-        //rotate(PI/5.0);
-        text(i,0,0);
-        n=(width - 2*marginSlider)/17+n;
-        pop();
-    }*/
+                        push();//display year
+                        fill(75);
+                        noStroke();
+                        translate(xPos,height-115);
+                        rotate(-PI/5.0);
+                        text(mouseYear,0,0 );
+                        pop();
+                    }
+                    c=c+30;
+                }
+                break;
+            }
+        }
+    }
 
 }
 
-
-function drawCurrent(mx){
-
+function drawData(e){
+    strokeCap(SQUARE);
+    zipCurrent=e.target.feature.properties.zip;
 }
-/*function drawSlider(){
-    var slider;
-    slider = createSlider(1998,2015,1998);
-    slider.position(canvasX+marginSlider, height-100);
-    slider.style('width', '850px');
-}*/
 
 //-------------------LEAFLET CONTROL-----------------------------------------
 var map = L.map('map').setView([36.1678467,-86.7975992],11);
@@ -182,19 +132,6 @@ info.update = function (props) {
 };
 
 info.addTo(map);
-
-
-// get color depending on population density value
-function getColor(d) {
-    return d > 1000 ? '#800026' :
-        d > 500  ? '#BD0026' :
-            d > 200  ? '#E31A1C' :
-                d > 100  ? '#FC4E2A' :
-                    d > 50   ? '#FD8D3C' :
-                        d > 20   ? '#FEB24C' :
-                            d > 10   ? '#FED976' :
-                                '#FFEDA0';
-}
 
 function style(feature) {
     return {
@@ -230,10 +167,6 @@ var geojson;
 function resetHighlight(e) {
     geojson.resetStyle(e.target);
     info.update();
-}
-
-function zoomToFeature(e) {
-    map.fitBounds(e.target.getBounds());
 }
 
 function onEachFeature(feature, layer) {
