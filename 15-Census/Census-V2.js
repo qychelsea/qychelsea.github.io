1cf139e84e1ca94698399f149107381ade59723e
qychelseaPC <31788061+qychelsea@users.noreply.github.com>
2017-11-03 04:52:34 -0400

    visual edit 2

--- 15-Census/Census-V2.js
+++ 15-Census/Census-V2.js
@@ -5,6 +5,7 @@ var zipCurrent;
 //var counter=0;
 var xGap=15;
 var lineLength=10;
+var deltaGain, deltaGainCat, deltaLoss=0,deltaLossCat, deltaTtl,deltaTtlP; //most changed since last year (category), since 1998, Overall (percentage)
 var myFont;
 
 function preload(){
@@ -12,7 +13,6 @@ function preload(){
         table [i] = loadTable('data/zbp'+i+'.csv', 'csv', 'header');
     }
     neighbourhood = loadTable('data/Neighbourhood.csv','csv','header');
-
     myFont = loadFont('assets/Steelworks Vintage Demo.otf');
 }
 
@@ -29,9 +29,6 @@ function sortData(){
         esTab[i]=table[i].getColumn("ESTAB");
         empSzes[i]=table[i].getColumn("EMPSZES");
         zipCode[i]=table[i].getColumn("zipcode");
-
-        console.log("esTab["+i+"]=",esTab[i]);//problem from 1998 on. data not matching
-        console.log("empSzes["+i+"]=",empSzes[i]);//problem from 1998 on.
     }
     for (i=1;i<=21;i++){
         neighZip = neighbourhood.getColumn("zip");
@@ -81,7 +78,7 @@ function draw(){
         for( i = 0;i<=8;i++){
             noStroke();
             fill(color(r,r,94,240));// fill(color(c,90,130,240));
-            rect(xPos+i*50,height - 55,8,8);
+            rect(xPos+i*53,height - 55,8,8);
             r=r-25;
         }
         fill(105);
@@ -91,7 +88,7 @@ function draw(){
         text("business size (number of employees)",xPos-2,height-65);
         pop();
         textSize(12);
-        text("1-4        5-9       10-19      20-49     50-99     100-249   250-499    500-999     1000+",xPos-2,height - 35);
+        text("1-4        5-9        10-19      20-49     50-99       100-249    250-499     500-999     1000+",xPos-2,height - 35);
     }
 
     //draw alpha
@@ -127,6 +124,8 @@ function draw(){
         mouseYear = Math.floor(map(mouseX,marginSlider, width-marginSlider-lineLength,1998,2015));
         for(n = 0;n<=esTab[mouseYear].length;n++){
             if (zipCode[mouseYear][n]===zipCurrent+']'){
+                deltaGain=0;
+                deltaLoss=0;
                 //var yPosMod=0;//Y Position Modifier
                 for(j = n+9;j>n;j--){//goes through 260-212
                     strokeWeight(strokeWeightCurrent);
@@ -134,10 +133,42 @@ function draw(){
                     textSize(15);
                     var yPosP=yPos;//previous yPos for for legend
 
+                    //calculate change
+                    if(mouseYear>1998){
+                        deltaGain=esTab[mouseYear][j]-esTab[mouseYear-1][j];
+                        if (deltaGain===0){deltaLoss=0;deltaGainCat='none';deltaLossCat='none';}
+                        if(deltaGain>0){
+                            if(empSzes[mouseYear][j]==212){deltaGainCat='1-4';}
+                            if(empSzes[mouseYear][j]==220){deltaGainCat='5-9';}
+                            if(empSzes[mouseYear][j]==230){deltaGainCat='10-19';}
+                            if(empSzes[mouseYear][j]==241){deltaGainCat='20-49';}
+                            if(empSzes[mouseYear][j]==242){deltaGainCat='50-99';}
+                            if(empSzes[mouseYear][j]==252){deltaGainCat='100-249';}
+                            if(empSzes[mouseYear][j]==254){deltaGainCat='250-499';}
+                            if(empSzes[mouseYear][j]==260){deltaGainCat='500-999';}
+                        }
+                        if (deltaGain<0){
+                            deltaLoss=Math.abs(deltaGain);
+                            if(empSzes[mouseYear][j]==212){deltaLossCat='1-4';}
+                            if(empSzes[mouseYear][j]==220){deltaLossCat='5-9';}
+                            if(empSzes[mouseYear][j]==230){deltaLossCat='10-19';}
+                            if(empSzes[mouseYear][j]==241){deltaLossCat='20-49';}
+                            if(empSzes[mouseYear][j]==242){deltaLossCat='50-99';}
+                            if(empSzes[mouseYear][j]==252){deltaLossCat='100-249';}
+                            if(empSzes[mouseYear][j]==254){deltaLossCat='250-499';}
+                            if(empSzes[mouseYear][j]==260){deltaLossCat='500-999';}
+
+                        }
+                    }
+
+
+
                     for (k=0;k<esTab[mouseYear][j];k++){
                         xPos=marginSlider+(mouseYear-1998)*(lineLength+xGap);
                         line(xPos,yPos,xPos+lineLength,yPos);
-                        if (Math.ceil(esTab[mouseYear][j]/2)===k){//display legend
+
+
+                        if (Math.floor(esTab[mouseYear][j]/2)===k){//display legend
                             var legendX=xPos+lineLength+2,legendY=yPos;
 
                             //draw leaders
@@ -167,23 +198,10 @@ function draw(){
                             yPosP=legendY;
                             //counter=1;
                         }
-/*
-                        push();
-                        noFill();stroke(255);
-                        beginShape();
-                        for(var v=0;v<=mouseYear;v++){
-                            curveVertex(trendX[mouseYear-1998],trendY[mouseYear-1998]);
-                        }
-                        console.log("trendX=",trendX);
-                        console.log("trendY=",trendY);
-                        endShape();
-                        pop();
-*/
-                        yPos=yPos-strokeWeightCurrent;
+                         yPos=yPos-strokeWeightCurrent;
                     }
                     r=r+25;
                     if(esTab[mouseYear][j]!==0){yPos=yPos-1;}
-
                 }
 
                 //draw pop-up chart
@@ -195,38 +213,89 @@ function draw(){
                 var popX,popY=300;
                 if (mouseYear<2009){
                     popX=xPos+70;
-                    rect(popX,popY,popX+120,popY+425);
+                    rect(popX,popY,popX+120,popY+450);
                 }
                 else {
                     popX=xPos-130;
-                    rect(popX,popY,popX+120,popY+425);
+                    rect(popX,popY,popX+120,popY+450);
                 }
-                if(mouseYear>1998){
+                if(mouseYear>=1998){
                     fill(75);
 
                     textStyle(BOLD);
-                    text(mouseYear,popX+10,popY+20);
+                    textSize(20);
+                    text(mouseYear,popX+10,popY+25);
+                    stroke(75);
+                    line(popX+10,popY+28,popX+41,popY+28);
+
+                    noStroke();
+                    textSize(15);
                     fill(100);
+
+                    text("total",popX+10,popY+55);
                     textStyle(NORMAL);
-                    text("total",popX+10,popY+50);
-
-                    text("most changed since last year",popX+10,popY+100);
-                    text("most change since 1998",popX+10,popY+150);
-
-                    var busTotal=0;
                     for (i=0;i<=esTab[mouseYear].length;i++){
-                        if(empSzes[mouseYear][i]===1){
-                            busTotal = busTotal+esTab[mouseYear][i];
-                            console.log("busTotal=",busTotal);
+                        if(zipCode[mouseYear][i]===zipCurrent+']'){
+                            text(esTab[mouseYear][i], popX+10, popY+70);
+                            if (mouseYear>1998){
+                                deltaTtl=esTab[mouseYear][i]-esTab[mouseYear-1][i];
+                                deltaTtlP=(esTab[mouseYear][i]-esTab[mouseYear-1][i])*100/esTab[mouseYear-1][i];
+                                deltaTtlP=deltaTtlP.toFixed(2);
+                            }
+                            break;
                         }
                     }
 
-                    text(busTotal,popX+10, popY+75);
+                    if (mouseYear>1998){
+                        if (deltaTtl>=0){text("year gain",popX+10,popY+105);fill(102, 153, 0);}
+                        if (deltaTtl<0){text("year loss",popX+10,popY+105);fill(153,51,51);}
+                        text(deltaTtl+" ("+deltaTtlP+"%)",popX+10,popY+120);
+
+                        fill(102, 153, 0);
+                        if(deltaGain>0){
+                            text(deltaGainCat + " employee firms",popX+10,popY+170);
+                            text("gains "+deltaGain  + " firm(s)",popX+10,popY+185);
+                        }
+
+                        if (deltaGain===0){
+                            text("-",popX+10,popY+170);
+                        }
+
+                        fill(153,51,51);
+                        if (deltaLoss>0){
+                            text(deltaLossCat + " employee firms",popX+10,popY+235);
+                            text("losses "+deltaLoss + " firm(s)",popX+10,popY+250);
+                        }
+
+
+                        if (deltaLoss===0){
+                            text("-",popX+10,popY+235);
+                        }
+
+                        fill(100);
+                        text("most gain",popX+10,popY+155);
+                        text("most loss",popX+10,popY+220);
+
+                        //
+                    }
+
+
+
+
+                    /*for (i=0;i<=esTab[mouseYear].length;i++){//yearly total for all zipcodes
+                       // var busTotal=0;
+                       // console.log("establength=",esTab[mouseYear].length);
+                      // console.log("empSzes[mouseYear][i]=",empSzes[mouseYear][i]);
+                        if(empSzes[mouseYear][i]==1){
+                            console.log("busTotal=",busTotal);
+                            busTotal = parseInt(busTotal)+ parseInt(esTab[mouseYear][i]);
+                        }
+                    }*/
                 }
 
                 pop();
 
-/*                push();//display year
+/*              push();//display year under bar
                 noStroke();
                 fill(135);
                 translate(xPos,height-marginBase+30);
