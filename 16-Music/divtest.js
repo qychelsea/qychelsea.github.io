73ddc1c52022537c076f59bc095cf3d6c93b6fa9
yqiu <qychelsea@hotmail.com>
2017-12-06 13:37:21 -0500

    graphics done

--- 16-Music/divtest.js
+++ 16-Music/divtest.js
@@ -1,12 +1,10 @@
-var button;
-
 function setup(){
+    createCanvas(500,100);
+    background(190);
 
+    push();
+    var changeMusic = createDiv(" <a href='http://qychelsea.github.io/16-Music/menu.html'>Change Music</a>");
+
+
+    pop();
 }
-
-function draw(){
-    button
-    button.onClick=drawGraphic();
-    button.parent(menu);
-}
-
