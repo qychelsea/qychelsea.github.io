95a05999427079f6e147ada1bb15d76c829b156a
qychelseaPC <31788061+qychelsea@users.noreply.github.com>
2018-11-14 18:10:31 -0500

    essay img upload

--- SensingPlace/CheckTime.js
+++ SensingPlace/CheckTime.js
@@ -1,6 +1,5 @@
 var keyCode;
-var h = hour();
-
+var h,d,n;
 
 
 function setup() {
@@ -13,8 +12,9 @@ function draw(){
     textAlign(CENTER);
     text("press up to exit station", width/2,height/2);
     var weekday = new Array();
-    var d = new Date();
-    var n = d.getUTCDay();
+    d = new Date();
+    n = d.getUTCDay();
+    h = hour();
     weekday[0] = "Sunday";
     weekday[1] = "Monday";
     weekday[2] = "Tuesday";
@@ -24,17 +24,24 @@ function draw(){
     weekday[6] = "Saturday";
     text("n="+n, width/2,height/2+20);
     text(weekday[n], width/2,height/2+40);
+    text("d="+d, width/2,height/2+60);
+    text("h="+h, width/2,height/2+80);
     //text("hour="+h,10,30);
 
 }
 
 function keyPressed(){
     if (keyCode === UP_ARROW){
-        if (n!==0&&n!==6&&h<9&&h<18){
+        text("it gets here",width/2,height/2+60);
+        if (n===0||n===6){
             //window.open("https://www.youraddress.com","_self")
-            window.open("essay-day.html","_self");
-        }else{
             window.open("essay-night.html","_self");
+        }else if (h>9&&h<18){
+            window.open("essay-day.html", "_self");
         }
+        else {
+            window.open("essay-night.html", "_self)")
+        }
+
     }
 }
