79998a5bba88e3621027591ebbe985eb92d751ff
qychelsea <31788061+qychelsea@users.noreply.github.com>
2018-12-01 23:21:07 -0500

    Add files via upload

--- SensingPlace/Redirect.js
+++ SensingPlace/Redirect.js
@@ -4,19 +4,14 @@ var img;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-}
-
-function draw(){
     d = new Date();
     n = d.getDay();
     h = hour();
 
-    if (n===0||n===6){
-        window.open("essay-night-cover.html","_self");
-    }else if (h>=9&&h<=18){
-        window.open("essay-day-cover.html", "_self");
+    if (h>=9&&h<=18){
+        window.open("essay-day-cover.html","_self");
+    }else {
+        window.open("essay-night-cover.html", "_self");
     }
-    else {
-        window.open("essay-night-cover.html","_self");
-    }
+
 }
